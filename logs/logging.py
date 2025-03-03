1e841494bd1452e6e8353e1bbfc46e45172a26df
DatTruonggg <truongminhdat07@gmail.com>
2025-03-03 11:50:42 +0700

    feat: Add logger config file

---
+++ logs/logging.py
@@ -0,0 +1,33 @@
+
+from typing import TypeVar
+
+import loguru
+
+from configs import config
+
+log_level = "DEBUG"
+log_format = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS zz}</green> | "
+    "<level>{level: <8}</level> | "
+    "<yellow>Line {line: >4} ({file}):</yellow> <b>{message}</b>"
+)
+
+_T_logoru_logger = TypeVar("_T_logoru_logger", bound=loguru._logger.Logger)
+
+
+def logger_handler(
+    use_log_file: bool = True, file: str = "./logs/logs_tracking/logging_file.log"
+) -> _T_logoru_logger:
+    if use_log_file:
+        loguru.logger.add(
+            file,
+            level=log_level,
+            format=log_format,
+            colorize=False,
+            backtrace=True,
+            diagnose=True,
+        )
+    return loguru.logger
+
+
+log = logger_handler(file=config.logging_file)
